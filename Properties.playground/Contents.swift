b2685c9563518a67a124096f3e90b27f64b0dc8a
ss42 <sanjay.shrestha1988@gmail.com>
2016-04-26 10:56:38 -0700

    first commit

--- Properties.playground/Contents.swift
+++ Properties.playground/Contents.swift
@@ -42,10 +42,27 @@ struct LocationTrack {
     var locations: [CLLocation]
     
     var length: CLLocationDistance {
-        // this function should sum up all the distances between the locations in the track
-        return 1000.0 // right now it just returns 1000 (in meters, which is one kilometer).
+        
+        var distance:Double = 0
+        if locations.count == 0 { return 0}
+        else if locations.count  == 1
+        {
+            distance = locations.first!.distanceFromLocation(locations.first!)
+            return distance // right now it just returns 1000 (in meters, which is one kilometer).
+
+        }
+        else{
+           // for location in locations.count as! [CLLocation]
+            for (var i = 0; i < locations.count-1; i += 1)
+            {
+                let fromLocation = locations[i]
+                let toLocation = locations[i + 1]
+                distance += fromLocation.distanceFromLocation(toLocation)
+
+            }
+            return distance/1000
+        }
     }
-    
 }
 
 
@@ -65,8 +82,14 @@ class LocationTrackTestSuite: XCTestCase {
         let expectedResult: CLLocationDistance = 0
         XCTAssertEqual(expectedResult, onePointTrack.length, "Single point track should have zero length.")
     }
-    
-    
+    func testLengthOfTrackWithTwoPoints() {
+        let sanfrancisco = CLLocation(latitude: 37.774929, longitude: -122.419416)
+        let oakland = CLLocation(latitude: 37.8044, longitude: -122.2711)
+        let moraga = CLLocation(latitude: 37.841347, longitude: -122.110135)
+        let twoPointTrack = LocationTrack(locations: [sanfrancisco, oakland, moraga])
+        let expectedResult: CLLocationDistance = 28.22065585368208
+        XCTAssertEqual(expectedResult, twoPointTrack.length, "Locations between two points.")
+    }
 }
 /*:
  The last bit of arcana is necessary to support the execution of unit tests in a playground, but isn't documented in [Apple's XCTest Library]( https://github.com/apple/swift-corelibs-xctest ). I gratefully acknowledge Stuart Sharpe for sharing it in his blog post, [TDD in Swift Playgrounds]( http://initwithstyle.net/2015/11/tdd-in-swift-playgrounds/ ). */
